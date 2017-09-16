5e6a5cd3ac80825c8ef1413af06a7137e5c1760d
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-09-16 17:37:28 +0300

    code review

--- Divisors.py
+++ Divisors.py
@@ -1,12 +1,7 @@
 #http://www.practicepython.org/exercise/2014/02/26/04-divisors.html
 #a=int(input("Choose a number: "))
-a=100
+a = 100
 
-x=list(range(2, a // 2 + 1))
+xs = range(2, a // 2 + 1)
 
-divisors= []
-
-for i in x:
-	if (a % i==0):
-		divisors.append(i)
-print(divisors)
+print([x for x in xs if a % x == 0])
--- String Lists.py
+++ String Lists.py
@@ -1,20 +1,6 @@
 #http://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 
 #a=input("Write a string: ")
-a="ababababca"
-i=0
-j= len(a) - 1
+a = "abba"
 
-check = True
-
-while i<=j:
-	if (a[i]!=a[j]):
-		check = False
-		break
-	else:
-		i+=1
-		j-=1
-if check:
-	print("This string is palindrom")
-else:
-	print("This string isnt palindrom")
+print("String is palindrome") if a == a[::-1] else print("Why u do dis :C")
