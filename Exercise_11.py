f5cb4e36221bad6ef085b1076a48ec1a8e4cf2de
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-09-19 08:35:15 +0300

    exercise 11 review

--- Exercise_11.py
+++ Exercise_11.py
@@ -1,25 +1,13 @@
 #http://www.practicepython.org/exercise/2014/04/16/11-check-primality-functions.html
 def is_prime(number):
-    '''Returns True for prime numbers, False otherwise'''
-    #Edge Cases
-    if number == 1:
-        prime = False
-    elif number == 2:
-        prime = True
-    #All other primes
-    else:
-        prime = True
-        for check_number in range(2, (number // 2)+1):
-            if number % check_number == 0:
-                prime = False
-                break
-    return prime
+    return not (
+        number < 2
+        or any(number % d == 0 for d in range(2, int(number / 2) + 1))
+    )
 
 
 a = int(input("Give me a number: "))
 
 check = is_prime(a)
-if check:
-    print("You number is prime")
-else:
-    print("You numer not a prime")
+
+print("Your number is {}a prime number.".format("" if check else "not "))
